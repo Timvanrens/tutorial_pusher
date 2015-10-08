b09a527655240e8f934ebadedc9fc08107fafbae
Timvanrens <timvanrens_15@hotmail.com>
2015-10-08 12:23:18 +0200

    v2

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define('APP_ID', 'vul hier app id in');
+define('APP_KEY', 'vul hier api_key in');
+define('APP_SECRET', 'vul hier secret in');
+
+define('CHANNEL_NAME', 'private-messages');
+?>
